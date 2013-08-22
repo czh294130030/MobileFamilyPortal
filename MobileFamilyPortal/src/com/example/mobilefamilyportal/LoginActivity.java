2d5816f037eed7de5d95f8e5014039bdfe5312db
U-chadcao-PC\Administrator <Administrator@chadcao-PC.(none)>
2013-08-22 20:56:28 +0800

    modify LoginActivity.java

--- MobileFamilyPortal/src/com/example/mobilefamilyportal/LoginActivity.java
+++ MobileFamilyPortal/src/com/example/mobilefamilyportal/LoginActivity.java
@@ -5,10 +5,10 @@ import android.os.Bundle;
 
 public class LoginActivity extends Activity {
 
-	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_login);
+	    
 	}
 }
