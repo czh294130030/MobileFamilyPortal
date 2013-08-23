9904c188e83d5b8dd133864d8df02220a7f83350
U-SUZSOFT\Chad.Cao <Chad.Cao@SZ11100025.suzsoft.com>
2013-08-23 14:27:47 +0800

    add edit delete

--- MobileFamilyPortal/src/com/example/base/BaseField.java
+++ MobileFamilyPortal/src/com/example/base/BaseField.java
@@ -3,4 +3,7 @@ package com.example.base;
 public class BaseField {
 	public static final String DATABASE_NAME = "MobileFamilyPortal";//数据库的名字
 	public static final int DATABASEVERSION = 1;//版本号
+	public static final int ADD=0;
+	public static final int EDIT=1;
+	public static final int DELETE=2;
 }
