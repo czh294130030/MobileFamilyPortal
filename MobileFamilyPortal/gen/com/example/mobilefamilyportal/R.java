8241c24ba1518c9490df47100bdd644f8a3c0807
U-SUZSOFT\Chad.Cao <Chad.Cao@SZ11100025.suzsoft.com>
2013-09-03 10:49:21 +0800

    20130903

--- MobileFamilyPortal/gen/com/example/mobilefamilyportal/R.java
+++ MobileFamilyPortal/gen/com/example/mobilefamilyportal/R.java
@@ -21,20 +21,7 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int add=0x7f020000;
-        public static final int alert=0x7f020001;
-        public static final int calender=0x7f020002;
-        public static final int card=0x7f020003;
-        public static final int create=0x7f020004;
-        public static final int dailyconsume=0x7f020005;
-        public static final int delete=0x7f020006;
-        public static final int edit=0x7f020007;
-        public static final int ic_launcher=0x7f020008;
-        public static final int info=0x7f020009;
-        public static final int login=0x7f02000a;
-        public static final int minus=0x7f02000b;
-        public static final int others=0x7f02000c;
-        public static final int userinfo=0x7f02000d;
+        public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
         public static final int accountEditText=0x7f080008;
