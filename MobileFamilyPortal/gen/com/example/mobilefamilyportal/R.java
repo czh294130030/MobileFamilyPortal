704a4abea490915e0d4db9ea5b46456c4be951b5
U-SUZSOFT\Chad.Cao <Chad.Cao@SZ11100025.suzsoft.com>
2013-09-03 15:09:06 +0800

    20130903

--- MobileFamilyPortal/gen/com/example/mobilefamilyportal/R.java
+++ MobileFamilyPortal/gen/com/example/mobilefamilyportal/R.java
@@ -10,6 +10,13 @@ package com.example.mobilefamilyportal;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int blue=0x7f040003;
+        public static final int button_mouseover_color=0x7f040004;
+        public static final int green=0x7f040002;
+        public static final int red=0x7f040001;
+        public static final int transparent=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,30 +24,35 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bank=0x7f020000;
+        public static final int dailyconsume=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int login=0x7f020003;
+        public static final int others=0x7f020004;
+        public static final int user=0x7f020005;
     }
     public static final class id {
-        public static final int accountEditText=0x7f080008;
-        public static final int accountTextView=0x7f08000e;
-        public static final int action_settings=0x7f080010;
-        public static final int imageButton1=0x7f080000;
-        public static final int imageButton2=0x7f080001;
-        public static final int imageButton4=0x7f080003;
-        public static final int imageButton5=0x7f080004;
-        public static final int passwordEditText=0x7f08000c;
-        public static final int textView1=0x7f080005;
-        public static final int textView2=0x7f080009;
-        public static final int textView3=0x7f08000b;
-        public static final int titleTextView=0x7f080007;
-        public static final int userIDTextView=0x7f08000d;
-        public static final int userInfoImageButton=0x7f080002;
-        public static final int userInfoListView=0x7f080006;
-        public static final int userNameTextView=0x7f08000f;
-        public static final int usernameEditText=0x7f08000a;
+        public static final int accountEditText=0x7f090008;
+        public static final int accountTextView=0x7f09000e;
+        public static final int action_settings=0x7f090010;
+        public static final int bankImageButton=0x7f090004;
+        public static final int dailyConsumeImageButton=0x7f090001;
+        public static final int loginImageButton=0x7f090002;
+        public static final int othersImageButton=0x7f090005;
+        public static final int passwordEditText=0x7f09000c;
+        public static final int textView1=0x7f090000;
+        public static final int textView2=0x7f090009;
+        public static final int textView3=0x7f09000b;
+        public static final int titleTextView=0x7f090007;
+        public static final int userIDTextView=0x7f09000d;
+        public static final int userInfoImageButton=0x7f090003;
+        public static final int userInfoListView=0x7f090006;
+        public static final int userNameTextView=0x7f09000f;
+        public static final int usernameEditText=0x7f09000a;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
@@ -50,32 +62,34 @@ public final class R {
         public static final int list_userinfo=0x7f030004;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int account=0x7f05000c;
-        public static final int account_required=0x7f050010;
-        public static final int action_settings=0x7f050001;
-        public static final int add_item=0x7f050004;
-        public static final int add_unsuccessfully=0x7f050013;
-        public static final int add_userinfo=0x7f050008;
-        public static final int app_name=0x7f050000;
-        public static final int cancel=0x7f05000b;
-        public static final int confirm_to_delete=0x7f050016;
-        public static final int delete_item=0x7f050006;
-        public static final int delete_unsuccessfully=0x7f050015;
-        public static final int edit_item=0x7f050005;
-        public static final int edit_userinfo=0x7f050009;
-        public static final int hello_world=0x7f050002;
-        public static final int ok=0x7f05000a;
-        public static final int password=0x7f05000e;
-        public static final int password_required=0x7f050012;
-        public static final int update_unsuccessfully=0x7f050014;
-        public static final int user_info=0x7f050003;
-        public static final int user_name=0x7f05000d;
-        public static final int userinfo=0x7f050007;
-        public static final int username_required=0x7f050011;
-        public static final int warm_prompt=0x7f05000f;
+        public static final int account=0x7f06000d;
+        public static final int account_required=0x7f060011;
+        public static final int action_settings=0x7f060002;
+        public static final int add_item=0x7f060005;
+        public static final int add_unsuccessfully=0x7f060014;
+        public static final int add_userinfo=0x7f060009;
+        public static final int app_name=0x7f060000;
+        public static final int cancel=0x7f06000c;
+        public static final int confirm_to_delete=0x7f060017;
+        public static final int delete_item=0x7f060007;
+        public static final int delete_unsuccessfully=0x7f060016;
+        public static final int edit_item=0x7f060006;
+        public static final int edit_userinfo=0x7f06000a;
+        public static final int hello_world=0x7f060003;
+        public static final int module_build=0x7f060018;
+        public static final int ok=0x7f06000b;
+        public static final int password=0x7f06000f;
+        public static final int password_required=0x7f060013;
+        public static final int update_unsuccessfully=0x7f060015;
+        public static final int user_info=0x7f060004;
+        public static final int user_name=0x7f06000e;
+        public static final int userinfo=0x7f060008;
+        public static final int username_required=0x7f060012;
+        public static final int warm_prompt=0x7f060010;
+        public static final int welcome=0x7f060001;
     }
     public static final class style {
         /** 
@@ -99,16 +113,16 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
         /**  主页菜单 
          */
-        public static final int MenuStyle=0x7f060003;
+        public static final int MenuStyle=0x7f070003;
         /**  标题样式 
          */
-        public static final int TitleStyle=0x7f060002;
+        public static final int TitleStyle=0x7f070002;
     }
 }
--- MobileFamilyPortal/src/com/example/mobilefamilyportal/MainActivity.java
+++ MobileFamilyPortal/src/com/example/mobilefamilyportal/MainActivity.java
@@ -5,18 +5,35 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private ImageButton dailyConsumeImageButton=null;
 	private ImageButton userInfoImageButton=null;
+	private ImageButton loginImageButton=null;
+	private ImageButton bankImageButton=null;
+	private ImageButton othersImageButton=null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		/*日常消费*/
+		dailyConsumeImageButton=(ImageButton)findViewById(R.id.dailyConsumeImageButton);
+		dailyConsumeImageButton.setOnTouchListener(onMyOnTouchListener);
+		dailyConsumeImageButton.setOnClickListener(onMyClickListener);
+		/*登录*/
+		loginImageButton=(ImageButton)findViewById(R.id.loginImageButton);
+		loginImageButton.setOnTouchListener(onMyOnTouchListener);
+		loginImageButton.setOnClickListener(onMyClickListener);
 		/*用户信息*/
 		userInfoImageButton=(ImageButton)findViewById(R.id.userInfoImageButton);
+		userInfoImageButton.setOnTouchListener(onMyOnTouchListener);
 		userInfoImageButton.setOnClickListener(new ImageButton.OnClickListener() {
 			
 			@Override
@@ -26,8 +43,35 @@ public class MainActivity extends Activity {
 				startActivity(intent);
 			}
 		});
+		/*银行*/
+		bankImageButton=(ImageButton)findViewById(R.id.bankImageButton);
+		bankImageButton.setOnTouchListener(onMyOnTouchListener);
+		bankImageButton.setOnClickListener(onMyClickListener);
+		/*其他*/
+		othersImageButton=(ImageButton)findViewById(R.id.othersImageButton);
+		othersImageButton.setOnTouchListener(onMyOnTouchListener);
+		othersImageButton.setOnClickListener(onMyClickListener);
 	}
-
+	/*显示模块正在建设*/
+	private OnClickListener onMyClickListener=new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			Toast.makeText(MainActivity.this, R.string.module_build, Toast.LENGTH_LONG)
+			.show();
+		}
+	};
+	/*按下、松开ImageButton修改ImageButton背景颜色*/
+	private OnTouchListener onMyOnTouchListener=new OnTouchListener() {
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			if(event.getAction()==MotionEvent.ACTION_DOWN){//按钮被按下
+				v.setBackgroundColor(getResources().getColor(R.color.button_mouseover_color));
+			}else if(event.getAction()==MotionEvent.ACTION_UP){//按钮被松开
+				v.setBackgroundColor(getResources().getColor(R.color.transparent));
+			}
+			return false;
+		}
+	};
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
