8c42fbed7bb430288e5bb5a4204f90c6f51e26bb
U-SUZSOFT\Chad.Cao <Chad.Cao@SZ11100025.suzsoft.com>
2013-09-12 14:24:05 +0800

    20130912

---
+++ MobileFamilyPortal/src/com/example/model/ParaDetail.java
@@ -0,0 +1,27 @@
+package com.example.model;
+
+public class ParaDetail {
+
+	private int detailID;
+	private String description;
+	private int infoID;
+	
+	public int getDetailID() {
+		return detailID;
+	}
+	public void setDetailID(int detailID) {
+		this.detailID = detailID;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getInfoID() {
+		return infoID;
+	}
+	public void setInfoID(int infoID) {
+		this.infoID = infoID;
+	}
+}
---
+++ MobileFamilyPortal/src/com/example/model/ParaInfo.java
@@ -0,0 +1,20 @@
+package com.example.model;
+
+public class ParaInfo {
+	
+	private int infoID;
+	private String description;
+	
+	public int getInfoID() {
+		return infoID;
+	}
+	public void setInfoID(int infoID) {
+		this.infoID = infoID;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
