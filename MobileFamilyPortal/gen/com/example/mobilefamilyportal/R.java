cf92680f109cec5044bba45f9773486de6ef624f
U-SUZSOFT\Chad.Cao <Chad.Cao@SZ11100025.suzsoft.com>
2013-09-12 13:43:15 +0800

    20130912

--- MobileFamilyPortal/gen/com/example/mobilefamilyportal/R.java
+++ MobileFamilyPortal/gen/com/example/mobilefamilyportal/R.java
@@ -75,11 +75,13 @@ public final class R {
         public static final int welcomeTextView=0x7f0a0005;
     }
     public static final class layout {
-        public static final int activity_login=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_userinfo=0x7f030002;
-        public static final int activity_userinfoop=0x7f030003;
-        public static final int list_userinfo=0x7f030004;
+        public static final int activity_bankcard=0x7f030000;
+        public static final int activity_bankcardop=0x7f030001;
+        public static final int activity_login=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int activity_userinfo=0x7f030004;
+        public static final int activity_userinfoop=0x7f030005;
+        public static final int list_userinfo=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f090000;
---
+++ MobileFamilyPortal/src/com/example/mobilefamilyportal/BankCardActivity.java
@@ -0,0 +1,7 @@
+package com.example.mobilefamilyportal;
+
+import android.app.Activity;
+
+public class BankCardActivity extends Activity {
+
+}
