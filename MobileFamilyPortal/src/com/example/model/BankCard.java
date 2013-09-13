98c68154282185f373c789f163e244a8110acbe9
U-SUZSOFT\Chad.Cao <Chad.Cao@SZ11100025.suzsoft.com>
2013-09-13 13:26:28 +0800

    20130913

---
+++ MobileFamilyPortal/src/com/example/model/BankCard.java
@@ -0,0 +1,41 @@
+package com.example.model;
+
+public class BankCard {
+
+	private int cardID;
+	private String cardNO;
+	private int userID;
+	private int bankID;
+	private int cityID;
+	
+	public int getCardID() {
+		return cardID;
+	}
+	public void setCardID(int cardID) {
+		this.cardID = cardID;
+	}
+	public String getCardNO() {
+		return cardNO;
+	}
+	public void setCardNO(String cardNO) {
+		this.cardNO = cardNO;
+	}
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	public int getBankID() {
+		return bankID;
+	}
+	public void setBankID(int bankID) {
+		this.bankID = bankID;
+	}
+	public int getCityID() {
+		return cityID;
+	}
+	public void setCityID(int cityID) {
+		this.cityID = cityID;
+	}
+}
