ad9395c3b236480ade61dfac5ad653e53c77ccbe
U-SUZSOFT\Chad.Cao <Chad.Cao@SZ11100025.suzsoft.com>
2013-10-11 16:55:16 +0800

    20131011

--- MobileFamilyPortal/src/com/example/base/BaseMethod.java
+++ MobileFamilyPortal/src/com/example/base/BaseMethod.java
@@ -16,6 +16,57 @@ import android.view.Menu;
 
 public class BaseMethod {
 	
+	/*根据年，月，日获取当前日期(October 11, 2013)*/
+	public static String getCurrentDate(int year, int month, int day){
+		String dateString="";
+		dateString=convertMonth(month)+" "+day+", "+year;
+		return dateString;
+	}
+	/*获取当前月(October)*/
+	private static String convertMonth(int month){
+		String monthString="";
+		switch (month) {
+		case 0:
+			monthString="January";
+			break;
+		case 1:
+			monthString="February";
+			break;
+		case 2:
+			monthString="March";
+			break;
+		case 3:
+			monthString="April";
+			break;
+		case 4:
+			monthString="May";
+			break;
+		case 5:
+			monthString="June";
+			break;
+		case 6:
+			monthString="July";
+			break;
+		case 7:
+			monthString="August";
+			break;
+		case 8:
+			monthString="Sepetember";
+			break;
+		case 9:
+			monthString="October";
+			break;
+		case 10:
+			monthString="Novenber";
+			break;
+		case 11:
+			monthString="December";
+			break;
+		default:
+			break;
+		}
+		return monthString;
+	}
 	/*显示提示信息*/
 	public static void showInformation(Context context, int title, int message){
 		new AlertDialog.Builder(context)
--- MobileFamilyPortal/src/com/example/mobilefamilyportal/DailyConsumeActivityOP.java
+++ MobileFamilyPortal/src/com/example/mobilefamilyportal/DailyConsumeActivityOP.java
@@ -1,31 +1,90 @@
 package com.example.mobilefamilyportal;
 
+import java.util.Calendar;
+import java.util.List;
+
+import com.example.base.BaseField;
+import com.example.base.BaseMethod;
+import com.example.dal.ParaDetailDAL;
+import com.example.model.KeyValue;
+import com.example.mycontrol.MyConsumeControl;
+
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class DailyConsumeActivityOP extends Activity {
 
-	
+	private TextView amountTextView=null;
+	private EditText dateEditText=null;
 	private ImageButton calendarImageButton=null;
+	private ImageButton addImageButton=null;
+	private LinearLayout consumeLinearLayout=null;
+	private int mYear;
+	private int mMonth;
+	private int mDay;
+	private List<KeyValue> items=null;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_dailyconsumeop);
 	    calendarImageButton=(ImageButton)findViewById(R.id.calendarImageButton);
-	    calendarImageButton.setOnTouchListener(new ImageButton.OnTouchListener() {
+	    calendarImageButton.setOnTouchListener(onMyOnTouchListener);
+	    addImageButton=(ImageButton)findViewById(R.id.addImageButton);
+	    addImageButton.setOnTouchListener(onMyOnTouchListener);
+	    amountTextView=(TextView)findViewById(R.id.amountEditText);
+	    dateEditText=(EditText)findViewById(R.id.dateEditText);
+	    consumeLinearLayout=(LinearLayout)findViewById(R.id.consumeLinearLayout);
+	    /* 获取当前日期
+	     * 默认显示当前日期
+	     * 选择日期按钮修改显示日期
+	     * */
+		Calendar calendar=Calendar.getInstance();
+		mYear=calendar.get(Calendar.YEAR);
+		mMonth=calendar.get(Calendar.MONTH);
+		mDay=calendar.get(Calendar.DAY_OF_MONTH);
+		dateEditText.setText(BaseMethod.getCurrentDate(mYear, mMonth, mDay));
+	    calendarImageButton.setOnClickListener(new ImageButton.OnClickListener() {
 			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				if(event.getAction()==MotionEvent.ACTION_DOWN){
-					v.setBackgroundColor(getResources().getColor(R.color.button_mouseover_color));
-				}else if(event.getAction()==MotionEvent.ACTION_UP){
-					v.setBackgroundColor(getResources().getColor(R.color.transparent));
-				}
-				return false;
+			public void onClick(View v) {
+				new DatePickerDialog(DailyConsumeActivityOP.this, new DatePickerDialog.OnDateSetListener() {  
+					@Override  
+					public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {  
+						dateEditText.setText(BaseMethod.getCurrentDate(year, monthOfYear, dayOfMonth));  
+					}  
+				}, mYear, mMonth, mDay).show();  
+			}
+		});
+	    /*添加MyConsumeControl控件*/
+	    ParaDetailDAL paraDetailDAL=new ParaDetailDAL(this);
+	    items=paraDetailDAL.queryKeyValueList("WHERE infoID="+BaseField.CONSUME_TYPE);
+	    paraDetailDAL.close();
+	    addImageButton.setOnClickListener(new ImageButton.OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				MyConsumeControl myConsumeControl=new MyConsumeControl(DailyConsumeActivityOP.this, null, items);
+				consumeLinearLayout.addView(myConsumeControl);
 			}
 		});
 	}
-
+	/*按下、松开ImageButton修改ImageButton背景颜色*/
+	private OnTouchListener onMyOnTouchListener=new OnTouchListener() {
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			if(event.getAction()==MotionEvent.ACTION_DOWN){//按钮被按下
+				v.setBackgroundColor(getResources().getColor(R.color.button_mouseover_color));
+			}else if(event.getAction()==MotionEvent.ACTION_UP){//按钮被松开
+				v.setBackgroundColor(getResources().getColor(R.color.transparent));
+			}
+			return false;
+		}
+	};
 }
